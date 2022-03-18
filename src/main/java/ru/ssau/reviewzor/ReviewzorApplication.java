0cd56d01521a7ad6ace4eb2f4465ebae79b5632c
Vladislav <vladikgud@mail.ru>
2022-03-19 00:07:44 +0400

    init

---
+++ src/main/java/ru/ssau/reviewzor/ReviewzorApplication.java
@@ -0,0 +1,13 @@
+package ru.ssau.reviewzor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewzorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewzorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ssau/reviewzor/ReviewzorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ssau.reviewzor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewzorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
