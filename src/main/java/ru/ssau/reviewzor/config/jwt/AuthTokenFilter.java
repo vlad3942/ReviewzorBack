2922ff1de3887e4f540161351bacab84b2892f86
Vladislav <vladikgud@mail.ru>
2022-03-20 15:12:11 +0400

    Added Postman Collection

--- src/main/java/ru/ssau/reviewzor/config/jwt/AuthTokenFilter.java
+++ src/main/java/ru/ssau/reviewzor/config/jwt/AuthTokenFilter.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -45,6 +44,8 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 
                 SecurityContextHolder.getContext().setAuthentication(token);
             }
+        } catch (IllegalStateException ex) {
+            log.error(ex.getMessage());
         } catch (Exception ex) {
             log.error(ex.getMessage(), ex);
         }
